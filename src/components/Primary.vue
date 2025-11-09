3d4f5f82624214982bb289e798dd86b8497be472
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-11-09 11:03:56 -0600

    feat: add link to info cards

--- src/components/Primary.vue
+++ src/components/Primary.vue
@@ -39,25 +39,29 @@
 				title="Proactive"
 				icon="lucide:handshake"
 				description="Prioritize finding something that suits you, not just what looks good on a profile."
-				class="md:absolute mb-2 mt-8 md:my-0 bg-orange-500 shadow-lg shadow-orange-500/50 left-1/24 top-1/7 min-w-50 w-1/6 motion-delay-600 hover:opacity-90 transform"
+				class="md:absolute mb-2 mt-8 md:my-0 bg-orange-500 shadow-lg shadow-orange-500/50 left-1/24 top-1/7 min-w-50 w-1/6 motion-delay-600 hover:opacity-90 transform hover:cursor-pointer"
+				@click="navigateTo('https://app.earth-app.com', { external: true })"
 			/>
 			<InfoCard
 				title="Personal"
 				icon="material-symbols:person-add-rounded"
 				description="Connect with culture, community, and creative outlets just for you."
-				class="md:absolute my-2 md:my-0 bg-blue-500 shadow-lg shadow-blue-500/50 right-1/24 top-1/6 min-w-50 w-1/6 motion-delay-1100 hover:opacity-90 transform"
+				class="md:absolute my-2 md:my-0 bg-blue-500 shadow-lg shadow-blue-500/50 right-1/24 top-1/6 min-w-50 w-1/6 motion-delay-1100 hover:opacity-90 transform hover:cursor-pointer"
+				@click="navigateTo('https://app.earth-app.com', { external: true })"
 			/>
 			<InfoCard
 				title="Private"
 				icon="charm:shield-tick"
 				description="Standard end-to-end encryption ensures you prioritize novelty over noise."
-				class="md:absolute my-2 md:my-0 bg-green-500 shadow-lg shadow-green-500/50 left-1/16 top-3/7 min-w-50 w-1/6 motion-delay-1600 hover:opacity-90 transform"
+				class="md:absolute my-2 md:my-0 bg-green-500 shadow-lg shadow-green-500/50 left-1/16 top-3/7 min-w-50 w-1/6 motion-delay-1600 hover:opacity-90 transform hover:cursor-pointer"
+				@click="navigateTo('https://app.earth-app.com', { external: true })"
 			/>
 			<InfoCard
 				title="Powerful"
 				icon="mdi:gear"
 				description="AI-powered features enchance your experience, keeping you engaged, informed, and connected."
-				class="md:absolute my-2 md:my-0 bg-purple-500 shadow-lg shadow-purple-500/50 right-1/48 top-5/13 min-w-50 w-1/6 motion-delay-2100 hover:opacity-90 transform"
+				class="md:absolute my-2 md:my-0 bg-purple-500 shadow-lg shadow-purple-500/50 right-1/48 top-5/13 min-w-50 w-1/6 motion-delay-2100 hover:opacity-90 transform hover:cursor-pointer"
+				@click="navigateTo('https://app.earth-app.com', { external: true })"
 			/>
 		</div>
 	</main>
