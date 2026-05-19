9db9d1ffede720847bb52d5858b2a89e0a285815
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-05-19 17:46:08 -0500

    chore: remove crowded links, migrate to `NuxtLink`, refine status bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,8 +5,8 @@
 	>
 		<div class="flex items-center justify-evenly">
 			<div class="flex items-center justify-center lg:mr-8 xl:mr-12">
-				<a
-					href="https://app.earth-app.com"
+				<NuxtLink
+					to="https://app.earth-app.com"
 					class="size-8"
 				>
 					<img
@@ -14,84 +14,77 @@
 						alt="Earth App Logo"
 						class="min-w-8 h-auto mr-3"
 					/>
-				</a>
-				<h1 class="hidden md:inline-block sm:text-sm mx-2">{{ SITE_NAME }}</h1>
+				</NuxtLink>
+				<h1 class="hidden md:inline-block sm:text-sm mx-2">{{ appConfig.name }}</h1>
 			</div>
-			<div class="flex flex-col items-center justify-center mx-4 sm:mx-8 md:mx-4 lg:mr-8">
-				<div class="flex items-center justify-center">
-					<h2 class="mx-4 text-xs lg:text-sm text-center text-gray-200">
-						An app for you to try new things and explore novelty around the world
-					</h2>
-				</div>
-				<div
-					class="mt-4 md:mt-0 items-center justify-items-center text-center grid grid-cols-3 md:grid-cols-6 gap-x-3 gap-y-2 text-xs lg:text-sm font-semibold *:hover:text-primary *:transition-all *:duration-300"
-				>
-					<a href="/docs">Docs</a>
-					<a href="/api">API</a>
-					<a href="/about">About</a>
-					<a href="/blog">Blog</a>
-					<a href="/tos">Terms of Service</a>
-					<a href="/privacy-policy">Privacy Policy</a>
-				</div>
+			<div class="flex items-center justify-center">
+				<h2 class="mx-4 text-xs lg:text-sm text-center font-medium text-gray-200">
+					Dig deep and explore the world around you.
+				</h2>
 			</div>
 		</div>
 		<div
-			class="ml-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-2 *:hover:text-primary *:transition-all *:duration-300"
+			class="ml-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-x-2 *:hover:text-primary *:transition-all *:duration-300"
 		>
-			<a
-				href="https://github.com/earth-app"
+			<NuxtLink
+				to="https://github.com/earth-app"
 				target="_blank"
+				external
 			>
-				<Icon
+				<UIcon
 					name="uil:github"
 					title="GitHub"
 					size="calc(10px + 1.1vw + 1.5vh)"
 				/>
-			</a>
-			<a
-				href="https://instagram.com/theearthapp"
+			</NuxtLink>
+			<NuxtLink
+				to="https://instagram.com/theearthapp"
 				target="_blank"
+				external
 			>
-				<Icon
+				<UIcon
 					name="uil:instagram"
 					title="Instagram"
 					size="calc(10px + 1.1vw + 1.5vh)"
 				/>
-			</a>
-			<a
-				href="https://twitter.com/the_earth_app"
+			</NuxtLink>
+			<NuxtLink
+				to="https://twitter.com/the_earth_app"
 				target="_blank"
+				external
 			>
-				<Icon
+				<UIcon
 					name="uil:twitter"
 					title="Twitter"
 					size="calc(10px + 1.1vw + 1.5vh)"
 				/>
-			</a>
-			<a
-				href="https://patreon.com/gmitch215"
+			</NuxtLink>
+			<NuxtLink
+				to="https://patreon.com/gmitch215"
 				target="_blank"
+				external
 			>
-				<Icon
+				<UIcon
 					name="mdi:patreon"
 					title="Patreon"
 					size="calc(10px + 1.1vw + 1.5vh)"
 				/>
-			</a>
-			<a
-				href="https://youtube.com/@the_earth_app"
+			</NuxtLink>
+			<NuxtLink
+				to="https://youtube.com/@the_earth_app"
 				target="_blank"
+				external
 			>
-				<Icon
+				<UIcon
 					name="uil:youtube"
 					title="YouTube"
 					size="calc(10px + 1.1vw + 1.5vh)"
 				/>
-			</a>
+			</NuxtLink>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { SITE_NAME } from '#imports';
+const appConfig = useAppConfig();
 </script>
